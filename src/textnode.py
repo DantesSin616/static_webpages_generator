51244a4e0668c053356001a34e99d52b74c5a78b
DantesSin616 <asimpleperson100@gmail.com>
2026-02-20 16:29:50 -0400

    classes created

--- src/textnode.py
+++ src/textnode.py
@@ -1,14 +1,30 @@
 from enum import Enum
 
+
+def main():
+    return True
+
 class TextType(Enum):
 
-    BOLD_TEXT = **Bold text**
-    ITALIC_TEXT = _Italic text_
+    BOLD_TEXT = "**Bold text**"
+    ITALIC_TEXT = "_Italic text_"
     CODE_TEXT = `Code Text`
     LINK = "[anchor](url)"
     IMAGES = "![alt text](url)"
 
 
 
-class TextNode():
+class TextNode(text, text_type, url):
 
+    self.text = text 
+    self.text_type = text_type
+    self.url = url
+
+    def __eq__(self):
+        if self == TextNode()
+            return True
+
+    def __repr__(self):
+        return 
+
+main()
