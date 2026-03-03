35ad261856278007e1578b812b53d7579b5baf29
DantesSin616 <asimpleperson100@gmail.com>
2026-03-03 16:23:36 -0400

    LeafNode added for the generation of tags a values

--- htmlnode.py
+++ htmlnode.py
@@ -23,8 +23,35 @@ class HtmlNode:
 
 
     def __repr__(self):
-        # return a descriptive representation instead of printing
+        # return a descriptive representation of what was inputed 
         return (
             f"HtmlNode(tag={self.tag!r}, value={self.value!r}, "
             f"children={self.children!r}, props={self.props!r})"
+        )
+
+
+class LeafNode(HtmlNode):
+    def __init__(self, tag, value, props=None):
+
+        super().__init__(tag, value, None, props) 
+        # we use the super() function to call the father's constructor
+        # so we inherited the attributes without repeting yourself 
+
+
+    def to_html(self):
+        # check for required value
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+
+        # if tag is None, returns it as flat text
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+    def __repr__(self):
+        return (
+            f"LeafNode(tag={self.tag!r}, value={self.value!r}, "
+            f"props={self.props!r})"
         )
